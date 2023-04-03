7e12cf6e6726afb11729f63dfadf828988655c1d
Anna Zhuk <annazhuk2067@gmail.com>
2023-04-03 11:45:39 +0300

    Удалила анимацию

--- aron/src/components/Content/TextBuild.vue
+++ aron/src/components/Content/TextBuild.vue
@@ -4,13 +4,6 @@ const Adjective = "fantastic"
 const WebflowText = "WEBFLOW WEBSITES"
 import '../styles/part2.css'
 </script>
-<script lang="ts">
-
-
-
-
-
-</script>
 <template>
     <div  class="FirstRowTextBuild">
         <h3 :src=BuildText class="BuildText">{{BuildText}}
@@ -20,19 +13,3 @@ import '../styles/part2.css'
     <h3 :src=WebflowText class="WebflowText">{{WebflowText}}</h3>
 </template>
 
-<style>
-.Adjective {
-  display: inline-block;
-  overflow: hidden;
-  animation: typing 4s steps(50, end) 1s forwards;
-}
-
-@keyframes typing {
-  from {
-    width: 0;
-  }
-  to {
-    width: 100%;
-  }
-}
-</style>
